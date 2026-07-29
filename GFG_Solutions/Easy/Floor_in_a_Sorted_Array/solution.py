e027573a949626207f43e4f9a59be0e647b56472
ADITYA <dosed0992@gmail.com>
2026-07-29 18:20:46 +0530

    Added: Floor in a Sorted Array (Solution)

---
+++ GFG_Solutions/Easy/Floor_in_a_Sorted_Array/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Floor in a Sorted Array
+Difficulty: Easy
+Problem Link: https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
