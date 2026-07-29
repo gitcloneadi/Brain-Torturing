8a8a42aac87e321903c8a5991de969240ac9bc62
ADITYA <dosed0992@gmail.com>
2026-07-29 18:26:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Modular Exponentiation/modular-exponentiation-for-large-numbers5537.py
@@ -0,0 +1,13 @@
+"""
+# [Modular Exponentiation](https://www.geeksforgeeks.org/problems/modular-exponentiation-for-large-numbers5537/0)
+# Difficulty Level : Difficulty: Medium
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
