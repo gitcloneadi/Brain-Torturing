fa71f6a6464e44483fdfeff76a88973ace93e661
ADITYA <dosed0992@gmail.com>
2026-07-29 18:38:19 +0530

    Added Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring1452.py
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring1452.py
@@ -4,9 +4,16 @@
 """
 
 class Solution:
-    def solve(self):
-        # TODO: Paste your accepted GeeksforGeeks solution here
-        pass
+    def longestCommonSubstr(self, s1, s2):
+        n, m = len(s1), len(s2)
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        res = 0
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                    res = max(res, dp[i][j])
+        return res
 
 if __name__ == "__main__":
     # Add your test cases here
