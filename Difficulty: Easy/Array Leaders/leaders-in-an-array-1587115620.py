882b4c63fc49fe2d83fa70f38a7ee4dffdafc37f
ADITYA <dosed0992@gmail.com>
2026-07-29 18:29:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/leaders-in-an-array-1587115620.py
@@ -0,0 +1,13 @@
+"""
+# [Array Leaders](https://www.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
