cbee55bbb1402a0a844dcf0f061cb6cb369c94b8
ADITYA <dosed0992@gmail.com>
2026-07-29 18:36:09 +0530

    Added Solution - GfG to GitHub

--- Difficulty: Easy/Least Prime Factor/least-prime-factor5216.py
+++ Difficulty: Easy/Least Prime Factor/least-prime-factor5216.py
@@ -4,9 +4,14 @@
 """
 
 class Solution:
-    def solve(self):
-        # TODO: Paste your accepted GeeksforGeeks solution here
-        pass
+    def leastPrimeFactor(self, n):
+        res = list(range(n + 1))
+        for i in range(2, int(n**0.5) + 1):
+            if res[i] == i:
+                for j in range(i * i, n + 1, i):
+                    if res[j] == j:
+                        res[j] = i
+        return res
 
 if __name__ == "__main__":
     # Add your test cases here
