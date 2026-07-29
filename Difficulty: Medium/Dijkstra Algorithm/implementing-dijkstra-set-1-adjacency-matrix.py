759d8df7b0eebbb70bbc27e9ad13a931a01ceff3
ADITYA <dosed0992@gmail.com>
2026-07-29 18:26:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Dijkstra Algorithm/implementing-dijkstra-set-1-adjacency-matrix.py
@@ -0,0 +1,13 @@
+"""
+# [Dijkstra Algorithm](https://www.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/0)
+# Difficulty Level : Difficulty: Medium
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
