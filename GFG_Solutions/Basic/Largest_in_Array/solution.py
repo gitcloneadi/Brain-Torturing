68888e18decad04825d25bbe6e84401c4893dff3
ADITYA <dosed0992@gmail.com>
2026-07-29 18:21:13 +0530

    Added: Largest in Array (Solution)

---
+++ GFG_Solutions/Basic/Largest_in_Array/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Largest in Array
+Difficulty: Basic
+Problem Link: https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
