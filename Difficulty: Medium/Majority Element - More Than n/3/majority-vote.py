74ff240ff6a688d07c53191cbdefe37d64b1e563
ADITYA <dosed0992@gmail.com>
2026-07-29 18:24:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element - More Than n/3/majority-vote.py
@@ -0,0 +1,13 @@
+"""
+# [Majority Element - More Than n/3](https://www.geeksforgeeks.org/problems/majority-vote/0)
+# Difficulty Level : Difficulty: Medium
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
