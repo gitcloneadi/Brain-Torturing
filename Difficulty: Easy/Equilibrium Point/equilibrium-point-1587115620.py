bc3b456114db8c96081acc98b11cb653910b9da2
ADITYA <dosed0992@gmail.com>
2026-07-29 18:28:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point-1587115620.py
@@ -0,0 +1,13 @@
+"""
+# [Equilibrium Point](https://www.geeksforgeeks.org/problems/equilibrium-point-1587115620/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
