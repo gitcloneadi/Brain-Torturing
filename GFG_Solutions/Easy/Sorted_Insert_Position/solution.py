9cc54ec6e05f074f4ac180d7aa09c7dca39a5ede
ADITYA <dosed0992@gmail.com>
2026-07-29 18:20:43 +0530

    Added: Sorted Insert Position (Solution)

---
+++ GFG_Solutions/Easy/Sorted_Insert_Position/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Sorted Insert Position
+Difficulty: Easy
+Problem Link: https://www.geeksforgeeks.org/problems/search-insert-position-of-k-in-a-sorted-array/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
