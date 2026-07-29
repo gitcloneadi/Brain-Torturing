594662a0071a7e5229bba9148e00312b57819b24
ADITYA <dosed0992@gmail.com>
2026-07-29 18:24:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/inversion-of-array-1587115620.py
@@ -0,0 +1,13 @@
+"""
+# [Count Inversions](https://www.geeksforgeeks.org/problems/inversion-of-array-1587115620/0)
+# Difficulty Level : Difficulty: Medium
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
