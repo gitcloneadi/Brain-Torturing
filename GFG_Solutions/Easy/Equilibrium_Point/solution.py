92504a13b5e87e46cf78dbf0737af2212b6e18ef
ADITYA <dosed0992@gmail.com>
2026-07-29 18:23:20 +0530

    Added: Equilibrium Point (Solution)

---
+++ GFG_Solutions/Easy/Equilibrium_Point/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Equilibrium Point
+Difficulty: Easy
+Problem Link: https://www.geeksforgeeks.org/problems/equilibrium-point-1587115620/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
