52909fe255cb0620c08c798cef8e8c00c6bee178
ADITYA <dosed0992@gmail.com>
2026-07-29 18:25:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Permutation/next-permutation5226.py
@@ -0,0 +1,13 @@
+"""
+# [Next Permutation](https://www.geeksforgeeks.org/problems/next-permutation5226/0)
+# Difficulty Level : Difficulty: Medium
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
