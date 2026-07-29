2d0e94e41fc3ffb9746966d547105936e3bb5bd5
ADITYA <dosed0992@gmail.com>
2026-07-29 18:21:17 +0530

    Added: Reverse a Linked List (Solution)

---
+++ GFG_Solutions/Easy/Reverse_a_Linked_List/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Reverse a Linked List
+Difficulty: Easy
+Problem Link: https://www.geeksforgeeks.org/problems/reverse-a-linked-list/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
