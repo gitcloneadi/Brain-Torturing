b6482861b3ff2fbc6f4e364dfaa0a88380636691
ADITYA <dosed0992@gmail.com>
2026-07-29 18:22:31 +0530

    Added: Longest Common Substring (Solution)

---
+++ GFG_Solutions/Medium/Longest_Common_Substring/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Longest Common Substring
+Difficulty: Medium
+Problem Link: https://www.geeksforgeeks.org/problems/longest-common-substring1452/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
