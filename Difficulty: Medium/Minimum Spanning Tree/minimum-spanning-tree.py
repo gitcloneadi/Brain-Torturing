c7a930ed6a3afc845296212ec58cc4e8e5af094d
ADITYA <dosed0992@gmail.com>
2026-07-29 18:36:31 +0530

    Added Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.py
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.py
@@ -3,10 +3,22 @@
 # Difficulty Level : Difficulty: Medium
 """
 
+import heapq
 class Solution:
-    def solve(self):
-        # TODO: Paste your accepted GeeksforGeeks solution here
-        pass
+    def spanningTree(self, V, adj):
+        visited = [False] * V
+        min_heap = [(0, 0)]
+        res = 0
+        while min_heap:
+            weight, u = heapq.heappop(min_heap)
+            if visited[u]:
+                continue
+            visited[u] = True
+            res += weight
+            for v, w in adj[u]:
+                if not visited[v]:
+                    heapq.heappush(min_heap, (w, v))
+        return res
 
 if __name__ == "__main__":
     # Add your test cases here
