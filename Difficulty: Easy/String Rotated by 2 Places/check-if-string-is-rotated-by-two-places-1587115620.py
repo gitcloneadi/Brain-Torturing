017486fbfcedde0323310ed34a504b4b8f0f86ea
ADITYA <dosed0992@gmail.com>
2026-07-29 18:27:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/String Rotated by 2 Places/check-if-string-is-rotated-by-two-places-1587115620.py
@@ -0,0 +1,13 @@
+"""
+# [String Rotated by 2 Places](https://www.geeksforgeeks.org/problems/check-if-string-is-rotated-by-two-places-1587115620/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
