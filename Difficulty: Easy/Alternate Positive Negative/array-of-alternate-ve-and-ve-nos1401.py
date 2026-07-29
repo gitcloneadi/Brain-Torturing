a4079f1003b72c5bb472e7490d95e696dcadd928
ADITYA <dosed0992@gmail.com>
2026-07-29 18:25:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Alternate Positive Negative/array-of-alternate-ve-and-ve-nos1401.py
@@ -0,0 +1,13 @@
+"""
+# [Alternate Positive Negative](https://www.geeksforgeeks.org/problems/array-of-alternate-ve-and-ve-nos1401/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
