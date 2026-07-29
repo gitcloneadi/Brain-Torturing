084bd17cce205dc0d473924d93ba572f0087c2b2
ADITYA <dosed0992@gmail.com>
2026-07-29 18:37:14 +0530

    Added Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.py
+++ Difficulty: Easy/Insertion Sort/insertion-sort.py
@@ -4,9 +4,14 @@
 """
 
 class Solution:
-    def solve(self):
-        # TODO: Paste your accepted GeeksforGeeks solution here
-        pass
+    def insertionSort(self, arr):
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+            while j >= 0 and arr[j] > key:
+                arr[j + 1] = arr[j]
+                j -= 1
+            arr[j + 1] = key
 
 if __name__ == "__main__":
     # Add your test cases here
