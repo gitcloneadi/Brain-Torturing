0a45d073a1797ebe31f4ba4ffd5b9af52b3be5bf
ADITYA <dosed0992@gmail.com>
2026-07-29 18:25:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.py
@@ -0,0 +1,13 @@
+"""
+# [Implement Lower Bound](https://www.geeksforgeeks.org/problems/implement-lower-bound/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
