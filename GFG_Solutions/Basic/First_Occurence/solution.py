2b863250ded7a57b9f8dbf5505e0edf22f9e6b85
ADITYA <dosed0992@gmail.com>
2026-07-29 18:22:13 +0530

    Added: First Occurence (Solution)

---
+++ GFG_Solutions/Basic/First_Occurence/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: First Occurence
+Difficulty: Basic
+Problem Link: https://www.geeksforgeeks.org/problems/implement-strstr/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
