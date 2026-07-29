728111f0658409f53b110d143c1f4ee4b79cb91f
ADITYA <dosed0992@gmail.com>
2026-07-29 18:29:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Occurrence in Sorted/binary-search-1587115620.py
@@ -0,0 +1,13 @@
+"""
+# [First Occurrence in Sorted](https://www.geeksforgeeks.org/problems/binary-search-1587115620/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
