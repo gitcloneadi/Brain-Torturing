ec0098a1647aef83d022f303fec501ca9457d1d2
ADITYA <dosed0992@gmail.com>
2026-07-29 18:22:17 +0530

    Added: Multiply Two Strings (Solution)

---
+++ GFG_Solutions/Medium/Multiply_Two_Strings/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Multiply Two Strings
+Difficulty: Medium
+Problem Link: https://www.geeksforgeeks.org/problems/multiply-two-strings/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
