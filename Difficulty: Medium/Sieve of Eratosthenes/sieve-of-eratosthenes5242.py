5a6dd09f50be5d295e4711962fd66d8e004b491b
ADITYA <dosed0992@gmail.com>
2026-07-29 18:36:24 +0530

    Added Solution - GfG to GitHub

--- Difficulty: Medium/Sieve of Eratosthenes/sieve-of-eratosthenes5242.py
+++ Difficulty: Medium/Sieve of Eratosthenes/sieve-of-eratosthenes5242.py
@@ -4,9 +4,16 @@
 """
 
 class Solution:
-    def solve(self):
-        # TODO: Paste your accepted GeeksforGeeks solution here
-        pass
+    def sieveOfEratosthenes(self, n):
+        if n < 2:
+            return []
+        sieve = [True] * (n + 1)
+        sieve[0] = sieve[1] = False
+        for i in range(2, int(n**0.5) + 1):
+            if sieve[i]:
+                for j in range(i * i, n + 1, i):
+                    sieve[j] = False
+        return [i for i in range(2, n + 1) if sieve[i]]
 
 if __name__ == "__main__":
     # Add your test cases here
