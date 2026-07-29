3aa3b1023114460983e55e8c146b547cdb89d84b
ADITYA <dosed0992@gmail.com>
2026-07-29 18:25:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted Insert Position/search-insert-position-of-k-in-a-sorted-array.py
@@ -0,0 +1,13 @@
+"""
+# [Sorted Insert Position](https://www.geeksforgeeks.org/problems/search-insert-position-of-k-in-a-sorted-array/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
