3b6a08f1f7c964fe8a5141128e89e6395fb34235
ADITYA <dosed0992@gmail.com>
2026-07-29 18:33:43 +0530

    Added Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element - More Than n/3/majority-vote.py
+++ Difficulty: Medium/Majority Element - More Than n/3/majority-vote.py
@@ -4,9 +4,27 @@
 """
 
 class Solution:
-    def solve(self):
-        # TODO: Paste your accepted GeeksforGeeks solution here
-        pass
+    def findMajority(self, arr):
+        n = len(arr)
+        cand1, cand2 = None, None
+        count1, count2 = 0, 0
+        for x in arr:
+            if cand1 == x:
+                count1 += 1
+            elif cand2 == x:
+                count2 += 1
+            elif count1 == 0:
+                cand1, count1 = x, 1
+            elif count2 == 0:
+                cand2, count2 = x, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+        res = []
+        for cand in [cand1, cand2]:
+            if cand is not None and arr.count(cand) > n // 3:
+                res.append(cand)
+        return sorted(res)
 
 if __name__ == "__main__":
     # Add your test cases here
