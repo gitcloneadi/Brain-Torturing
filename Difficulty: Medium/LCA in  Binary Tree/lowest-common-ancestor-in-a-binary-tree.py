ef8727a19b76be9055336dca75e669ca4b4b7781
ADITYA <dosed0992@gmail.com>
2026-07-29 18:26:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/LCA in  Binary Tree/lowest-common-ancestor-in-a-binary-tree.py
@@ -0,0 +1,13 @@
+"""
+# [LCA in  Binary Tree](https://www.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-binary-tree/0)
+# Difficulty Level : Difficulty: Medium
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
