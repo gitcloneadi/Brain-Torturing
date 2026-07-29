70b38d030d8e51b229a3e2be5b7cf8b498b6db39
ADITYA <dosed0992@gmail.com>
2026-07-29 18:35:28 +0530

    Added Solution - GfG to GitHub

--- Difficulty: Medium/LCA in  Binary Tree/lowest-common-ancestor-in-a-binary-tree.py
+++ Difficulty: Medium/LCA in  Binary Tree/lowest-common-ancestor-in-a-binary-tree.py
@@ -4,9 +4,14 @@
 """
 
 class Solution:
-    def solve(self):
-        # TODO: Paste your accepted GeeksforGeeks solution here
-        pass
+    def lca(self, root, n1, n2):
+        if not root or root.data == n1 or root.data == n2:
+            return root
+        left = self.lca(root.left, n1, n2)
+        right = self.lca(root.right, n1, n2)
+        if left and right:
+            return root
+        return left if left else right
 
 if __name__ == "__main__":
     # Add your test cases here
