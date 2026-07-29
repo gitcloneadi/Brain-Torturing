ac50ce0c3257155a89286a4026e6cac600163b02
ADITYA <dosed0992@gmail.com>
2026-07-29 18:26:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/check-if-linked-list-is-pallindrome.py
@@ -0,0 +1,13 @@
+"""
+# [Palindrome Linked List](https://www.geeksforgeeks.org/problems/check-if-linked-list-is-pallindrome/0)
+# Difficulty Level : Difficulty: Medium
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
