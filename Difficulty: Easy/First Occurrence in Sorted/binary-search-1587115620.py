229661d92bc1dfcd035bf719b41b0aab654dce3a
ADITYA <dosed0992@gmail.com>
2026-07-29 18:40:25 +0530

    Added Solution - GfG to GitHub

--- Difficulty: Easy/First Occurrence in Sorted/binary-search-1587115620.py
+++ Difficulty: Easy/First Occurrence in Sorted/binary-search-1587115620.py
@@ -4,9 +4,19 @@
 """
 
 class Solution:
-    def solve(self):
-        # TODO: Paste your accepted GeeksforGeeks solution here
-        pass
+    def firstOccurrence(self, arr, target):
+        low, high = 0, len(arr) - 1
+        res = -1
+        while low <= high:
+            mid = (low + high) // 2
+            if arr[mid] == target:
+                res = mid
+                high = mid - 1
+            elif arr[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return res
 
 if __name__ == "__main__":
     # Add your test cases here
