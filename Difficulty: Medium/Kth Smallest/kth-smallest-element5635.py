74882e9b10f6cfca8bccc0791a864931d6091b5d
ADITYA <dosed0992@gmail.com>
2026-07-29 18:28:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Smallest/kth-smallest-element5635.py
@@ -0,0 +1,13 @@
+"""
+# [Kth Smallest](https://www.geeksforgeeks.org/problems/kth-smallest-element5635/0)
+# Difficulty Level : Difficulty: Medium
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
