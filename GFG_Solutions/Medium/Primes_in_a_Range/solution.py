ab64539b7213b5f4abf2bac46b1f043c320c5286
ADITYA <dosed0992@gmail.com>
2026-07-29 18:21:33 +0530

    Added: Primes in a Range (Solution)

---
+++ GFG_Solutions/Medium/Primes_in_a_Range/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Primes in a Range
+Difficulty: Medium
+Problem Link: https://www.geeksforgeeks.org/problems/find-prime-numbers-in-a-range4718/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
