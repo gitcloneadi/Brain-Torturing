c06769c79cee4ed83cb31d134c4e06843d23f641
ADITYA <dosed0992@gmail.com>
2026-07-29 18:28:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/buy-stock-2.py
@@ -0,0 +1,13 @@
+"""
+# [Stock Buy and Sell – Max one Transaction Allowed](https://www.geeksforgeeks.org/problems/buy-stock-2/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
