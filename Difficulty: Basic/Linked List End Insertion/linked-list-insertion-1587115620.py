e488ffb87ea2e13177ce4b3a8857130d131e7132
ADITYA <dosed0992@gmail.com>
2026-07-29 18:28:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Linked List End Insertion/linked-list-insertion-1587115620.py
@@ -0,0 +1,13 @@
+"""
+# [Linked List End Insertion](https://www.geeksforgeeks.org/problems/linked-list-insertion-1587115620/0)
+# Difficulty Level : Difficulty: Basic
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
