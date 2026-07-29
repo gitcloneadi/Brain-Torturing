2e1a3782116db9feafbd9ee97938f2dd1853c5f0
ADITYA <dosed0992@gmail.com>
2026-07-29 18:21:30 +0530

    Added: Least Prime Factor (Solution)

---
+++ GFG_Solutions/Easy/Least_Prime_Factor/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Least Prime Factor
+Difficulty: Easy
+Problem Link: https://www.geeksforgeeks.org/problems/least-prime-factor5216/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
