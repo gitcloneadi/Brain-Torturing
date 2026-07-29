73a1b558b395bbc553c124746ea8d7f85a5f8c17
ADITYA <dosed0992@gmail.com>
2026-07-29 18:23:17 +0530

    Added: Linked List End Insertion (Solution)

---
+++ GFG_Solutions/Basic/Linked_List_End_Insertion/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Linked List End Insertion
+Difficulty: Basic
+Problem Link: https://www.geeksforgeeks.org/problems/linked-list-insertion-1587115620/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
