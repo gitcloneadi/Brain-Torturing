d129a5e13cb1689bbc37bc4889ad94733cb9f2a8
ADITYA <dosed0992@gmail.com>
2026-07-29 18:35:21 +0530

    Added Solution - GfG to GitHub

--- Difficulty: Easy/Alternate Positive Negative/array-of-alternate-ve-and-ve-nos1401.py
+++ Difficulty: Easy/Alternate Positive Negative/array-of-alternate-ve-and-ve-nos1401.py
@@ -4,9 +4,25 @@
 """
 
 class Solution:
-    def solve(self):
-        # TODO: Paste your accepted GeeksforGeeks solution here
-        pass
+    def rearrange(self, arr):
+        pos = [x for x in arr if x >= 0]
+        neg = [x for x in arr if x < 0]
+        i = j = k = 0
+        while i < len(pos) and j < len(neg):
+            arr[k] = pos[i]
+            k += 1
+            i += 1
+            arr[k] = neg[j]
+            k += 1
+            j += 1
+        while i < len(pos):
+            arr[k] = pos[i]
+            k += 1
+            i += 1
+        while j < len(neg):
+            arr[k] = neg[j]
+            k += 1
+            j += 1
 
 if __name__ == "__main__":
     # Add your test cases here
