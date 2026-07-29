d00defea540229cfd468e329362486e236f5b45c
ADITYA <dosed0992@gmail.com>
2026-07-29 18:20:19 +0530

    Added: Majority Element - More Than n/3 (Solution)

---
+++ GFG_Solutions/Medium/Majority_Element_-_More_Than_n3/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Majority Element - More Than n/3
+Difficulty: Medium
+Problem Link: https://www.geeksforgeeks.org/problems/majority-vote/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
