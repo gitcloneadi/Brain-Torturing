7a93e0b39ed390e887704d3e13ac91f7b46ed13d
ADITYA <dosed0992@gmail.com>
2026-07-29 18:29:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Second Largest/second-largest3735.py
@@ -0,0 +1,13 @@
+"""
+# [Second Largest](https://www.geeksforgeeks.org/problems/second-largest3735/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
