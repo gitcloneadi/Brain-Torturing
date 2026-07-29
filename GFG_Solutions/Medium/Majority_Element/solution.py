023d3b9ec6d99dca746db35f86ad40ca9488a652
ADITYA <dosed0992@gmail.com>
2026-07-29 18:22:51 +0530

    Added: Majority Element (Solution)

---
+++ GFG_Solutions/Medium/Majority_Element/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Majority Element
+Difficulty: Medium
+Problem Link: https://www.geeksforgeeks.org/problems/majority-element-1587115620/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
