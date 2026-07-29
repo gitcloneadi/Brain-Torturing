bd9a79f1b3b9243e4717f1203f2ecc8bb3e4a838
ADITYA <dosed0992@gmail.com>
2026-07-29 18:25:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Kth Rotation/rotation4723.py
@@ -0,0 +1,13 @@
+"""
+# [Find Kth Rotation](https://www.geeksforgeeks.org/problems/rotation4723/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
