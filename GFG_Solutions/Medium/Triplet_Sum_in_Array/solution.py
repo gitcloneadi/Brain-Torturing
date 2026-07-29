9f7721dba8edcaa84deb4f1edaafa46f3228d4c4
ADITYA <dosed0992@gmail.com>
2026-07-29 18:20:53 +0530

    Added: Triplet Sum in Array (Solution)

---
+++ GFG_Solutions/Medium/Triplet_Sum_in_Array/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Triplet Sum in Array
+Difficulty: Medium
+Problem Link: https://www.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
