93b60ec41127ba76aea6996135a84fc7ad9e4e64
ADITYA <dosed0992@gmail.com>
2026-07-29 18:26:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/BFS of graph/bfs-traversal-of-graph.py
@@ -0,0 +1,13 @@
+"""
+# [BFS of graph](https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
