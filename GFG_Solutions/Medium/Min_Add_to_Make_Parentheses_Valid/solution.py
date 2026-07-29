528fb53e48e601bf6bff2e7c3b7e38529ee2db8d
ADITYA <dosed0992@gmail.com>
2026-07-29 18:22:57 +0530

    Added: Min Add to Make Parentheses Valid (Solution)

---
+++ GFG_Solutions/Medium/Min_Add_to_Make_Parentheses_Valid/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Min Add to Make Parentheses Valid
+Difficulty: Medium
+Problem Link: https://www.geeksforgeeks.org/problems/min-add-to-make-parentheses-valid/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
