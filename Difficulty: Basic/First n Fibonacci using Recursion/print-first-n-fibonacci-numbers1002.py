e1554460af622e2a3f2cfb954267d546b602323e
ADITYA <dosed0992@gmail.com>
2026-07-29 18:28:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/First n Fibonacci using Recursion/print-first-n-fibonacci-numbers1002.py
@@ -0,0 +1,13 @@
+"""
+# [First n Fibonacci using Recursion](https://www.geeksforgeeks.org/problems/print-first-n-fibonacci-numbers1002/0)
+# Difficulty Level : Difficulty: Basic
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
