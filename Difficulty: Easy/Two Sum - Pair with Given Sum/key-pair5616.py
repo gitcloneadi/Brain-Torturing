ab1c3b02ac846a49fa2947fce24e0d7424fe74a5
ADITYA <dosed0992@gmail.com>
2026-07-29 18:25:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/key-pair5616.py
@@ -0,0 +1,13 @@
+"""
+# [Two Sum - Pair with Given Sum](https://www.geeksforgeeks.org/problems/key-pair5616/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
