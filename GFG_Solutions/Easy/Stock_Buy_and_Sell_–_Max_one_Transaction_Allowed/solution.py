699aa62b256996eff6273118487cf17c5831274c
ADITYA <dosed0992@gmail.com>
2026-07-29 18:23:07 +0530

    Added: Stock Buy and Sell – Max one Transaction Allowed (Solution)

---
+++ GFG_Solutions/Easy/Stock_Buy_and_Sell_–_Max_one_Transaction_Allowed/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Stock Buy and Sell – Max one Transaction Allowed
+Difficulty: Easy
+Problem Link: https://www.geeksforgeeks.org/problems/buy-stock-2/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
