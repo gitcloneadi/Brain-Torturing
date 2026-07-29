d3bde4bc4f52620c4bb46a5efd2ea534e027ccb4
ADITYA <dosed0992@gmail.com>
2026-07-29 18:34:50 +0530

    Added Solution - GfG to GitHub

--- Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array-1587115621.py
+++ Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array-1587115621.py
@@ -4,9 +4,20 @@
 """
 
 class Solution:
-    def solve(self):
-        # TODO: Paste your accepted GeeksforGeeks solution here
-        pass
+    def find3Numbers(self, arr, target):
+        arr.sort()
+        n = len(arr)
+        for i in range(n - 2):
+            left, right = i + 1, n - 1
+            while left < right:
+                curr_sum = arr[i] + arr[left] + arr[right]
+                if curr_sum == target:
+                    return True
+                elif curr_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+        return False
 
 if __name__ == "__main__":
     # Add your test cases here
