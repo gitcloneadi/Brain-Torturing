e0861dd131de6aab3091f4bbb8a8d1b165343b51
ADITYA <dosed0992@gmail.com>
2026-07-29 18:27:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Longest Common Prefix of Strings/longest-common-prefix-in-an-array5129.py
@@ -0,0 +1,13 @@
+"""
+# [Longest Common Prefix of Strings](https://www.geeksforgeeks.org/problems/longest-common-prefix-in-an-array5129/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
