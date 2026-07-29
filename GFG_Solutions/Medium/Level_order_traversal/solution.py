5ca69c7bcadf1e0cd1c8df687f5fa19ca0329de6
ADITYA <dosed0992@gmail.com>
2026-07-29 18:21:50 +0530

    Added: Level order traversal (Solution)

---
+++ GFG_Solutions/Medium/Level_order_traversal/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Level order traversal
+Difficulty: Medium
+Problem Link: https://www.geeksforgeeks.org/problems/level-order-traversal/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
