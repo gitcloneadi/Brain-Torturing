b10d836be99a609d80cf48cbbc1b4885defc91b3
ADITYA <dosed0992@gmail.com>
2026-07-29 18:27:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/First Occurence/implement-strstr.py
@@ -0,0 +1,13 @@
+"""
+# [First Occurence](https://www.geeksforgeeks.org/problems/implement-strstr/0)
+# Difficulty Level : Difficulty: Basic
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
