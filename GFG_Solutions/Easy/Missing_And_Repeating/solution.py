069026c860240fd618570fed204975a901648168
ADITYA <dosed0992@gmail.com>
2026-07-29 18:23:24 +0530

    Added: Missing And Repeating (Solution)

---
+++ GFG_Solutions/Easy/Missing_And_Repeating/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Missing And Repeating
+Difficulty: Easy
+Problem Link: https://www.geeksforgeeks.org/problems/find-missing-and-repeating2512/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
