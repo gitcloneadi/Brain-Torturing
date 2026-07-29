fe5970c5659acc4ef7755a9a13a749923ab4166a
ADITYA <dosed0992@gmail.com>
2026-07-29 18:26:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest in Array/largest-element-in-array4009.py
@@ -0,0 +1,13 @@
+"""
+# [Largest in Array](https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0)
+# Difficulty Level : Difficulty: Basic
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
