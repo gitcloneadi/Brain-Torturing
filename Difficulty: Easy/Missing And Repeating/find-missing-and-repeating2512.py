92a15f9b92706b9c4e4b186d2886616b96824377
ADITYA <dosed0992@gmail.com>
2026-07-29 18:28:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing And Repeating/find-missing-and-repeating2512.py
@@ -0,0 +1,13 @@
+"""
+# [Missing And Repeating](https://www.geeksforgeeks.org/problems/find-missing-and-repeating2512/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
