802b04168378c884488444439d458d0aa84dbec2
ADITYA <dosed0992@gmail.com>
2026-07-29 18:21:10 +0530

    Added: LCA in  Binary Tree (Solution)

---
+++ GFG_Solutions/Medium/LCA_in__Binary_Tree/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: LCA in  Binary Tree
+Difficulty: Medium
+Problem Link: https://www.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-binary-tree/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
