7ec251d02ee647e5bb8199fa492c0a15e653ef36
ADITYA <dosed0992@gmail.com>
2026-07-29 18:25:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted and Rotated Minimum/minimum-element-in-a-sorted-and-rotated-array3611.py
@@ -0,0 +1,13 @@
+"""
+# [Sorted and Rotated Minimum](https://www.geeksforgeeks.org/problems/minimum-element-in-a-sorted-and-rotated-array3611/0)
+# Difficulty Level : Difficulty: Easy
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
