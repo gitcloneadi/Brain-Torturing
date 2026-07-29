d9844cd32d5808a7b0ea6edae3c78ce0750a41ad
ADITYA <dosed0992@gmail.com>
2026-07-29 18:25:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First and Last in Sorted/first-and-last-occurrences-of-x3116.py
@@ -0,0 +1,13 @@
+"""
+# [First and Last in Sorted](https://www.geeksforgeeks.org/problems/first-and-last-occurrences-of-x3116/0)
+# Difficulty Level : Difficulty: Medium
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
