ca6afbd842c053ac985837575605a9f2dc82763a
ADITYA <dosed0992@gmail.com>
2026-07-29 18:25:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree-1587115621.py
@@ -0,0 +1,13 @@
+"""
+# [Rotate by 90 degree](https://www.geeksforgeeks.org/problems/rotate-by-90-degree-1587115621/0)
+# Difficulty Level : Difficulty: Medium
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
