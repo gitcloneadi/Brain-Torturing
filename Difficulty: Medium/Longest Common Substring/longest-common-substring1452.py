479b6f812d2b722a459847a2ebeaf4bfc670c2c1
ADITYA <dosed0992@gmail.com>
2026-07-29 18:27:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring1452.py
@@ -0,0 +1,13 @@
+"""
+# [Longest Common Substring](https://www.geeksforgeeks.org/problems/longest-common-substring1452/0)
+# Difficulty Level : Difficulty: Medium
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
