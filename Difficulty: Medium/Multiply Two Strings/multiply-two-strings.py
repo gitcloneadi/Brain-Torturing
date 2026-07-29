d2082570fcb32f07ea88fd268a3c8bbc3279d70a
ADITYA <dosed0992@gmail.com>
2026-07-29 18:27:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Multiply Two Strings/multiply-two-strings.py
@@ -0,0 +1,13 @@
+"""
+# [Multiply Two Strings](https://www.geeksforgeeks.org/problems/multiply-two-strings/0)
+# Difficulty Level : Difficulty: Medium
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
