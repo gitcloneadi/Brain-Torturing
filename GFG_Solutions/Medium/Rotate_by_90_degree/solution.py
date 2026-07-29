04aac6756b05211c1f5f198f6fd87202fbf79bd6
ADITYA <dosed0992@gmail.com>
2026-07-29 18:21:00 +0530

    Added: Rotate by 90 degree (Solution)

---
+++ GFG_Solutions/Medium/Rotate_by_90_degree/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Rotate by 90 degree
+Difficulty: Medium
+Problem Link: https://www.geeksforgeeks.org/problems/rotate-by-90-degree-1587115621/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
