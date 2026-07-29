a7947974c75c5a31af39d64611d81f1d3c1e9327
ADITYA <dosed0992@gmail.com>
2026-07-29 18:37:44 +0530

    Added Solution - GfG to GitHub

--- Difficulty: Basic/First Occurence/implement-strstr.py
+++ Difficulty: Basic/First Occurence/implement-strstr.py
@@ -4,9 +4,8 @@
 """
 
 class Solution:
-    def solve(self):
-        # TODO: Paste your accepted GeeksforGeeks solution here
-        pass
+    def firstOccurence(self, txt, pat):
+        return txt.find(pat)
 
 if __name__ == "__main__":
     # Add your test cases here
