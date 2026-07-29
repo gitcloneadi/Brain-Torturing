77e10a5a7bd885fe2311915be1c58dc1aefeb394
ADITYA <dosed0992@gmail.com>
2026-07-29 18:26:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Primes in a Range/find-prime-numbers-in-a-range4718.py
@@ -0,0 +1,13 @@
+"""
+# [Primes in a Range](https://www.geeksforgeeks.org/problems/find-prime-numbers-in-a-range4718/0)
+# Difficulty Level : Difficulty: Medium
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
