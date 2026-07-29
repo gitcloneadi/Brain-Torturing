7423dd2e4bdc7deaee575c9e5e2293c88300ac8d
ADITYA <dosed0992@gmail.com>
2026-07-29 18:20:40 +0530

    Added: First and Last in Sorted (Solution)

---
+++ GFG_Solutions/Medium/First_and_Last_in_Sorted/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: First and Last in Sorted
+Difficulty: Medium
+Problem Link: https://www.geeksforgeeks.org/problems/first-and-last-occurrences-of-x3116/0
+"""
+
+class Solution:
+    def solve(self):
+        # TODO: Paste your accepted GeeksforGeeks solution here
+        pass
+
+if __name__ == "__main__":
+    # Add your test cases here
+    pass
