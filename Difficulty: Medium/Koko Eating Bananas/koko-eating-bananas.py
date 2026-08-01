5562523d75219864ab746efac7fce826e4e1542b
ADITYA <dosed0992@gmail.com>
2026-08-01 13:47:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.py
@@ -0,0 +1,22 @@
+class Solution:
+    
+    def check(self, arr, mid, k):
+        total=0
+        for i in range(len(arr)):
+            total += (arr[i] + mid - 1) // mid
+        return total <= k
+        
+    
+    def kokoEat(self, arr, k):
+        # Code here
+        low = 1
+        high = max(arr)
+        res = high
+        while low <= high:
+            mid = low + (high-low)//2
+            if self.check(arr, mid, k) == True:
+                high = mid - 1
+                res = mid
+            else:
+                low = mid + 1
+        return res
