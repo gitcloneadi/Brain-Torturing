257332af82610fbe6183b7244da780a20acbdcc0
ADITYA <dosed0992@gmail.com>
2026-08-08 18:07:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mergeArrays(self, a, b):
+        # code here
+        i =len(a) - 1
+        j = 0
+        
+        while i >= 0 and j < len(b):
+            if a[i] < b[j]:
+                i-=1
+            else:
+                a[i], b[j] = b[j], a[i]
+                i-=1
+                j+=1
+        return a.sort() , b.sort()
+       
